b5d1afd42696dd2d711138bc61727a83f827a710
Hyein <hyeinu65@gmail.com>
2016-10-03 17:11:25 -0400

    working on the spyscroll navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,7 +12,7 @@ export default class NavBar extends Component{
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
-          <a className="navbar-brand" href="#">Brand</a>
+          <a className="navbar-brand" href="#">Home</a>
         </div>
         <div className="collapse navbar-collapse" id="mynavbar">
           <ul className="nav navbar-nav">
